7aae3e922e6c6d1d4740363c49154f245973a0af
zhangtao <flashpig8014@163.com>
2023-12-08 13:13:22 +0800

    处理read timeout

---
+++ src/main/java/io/netty/channel/jsc/JSerialCommReadTimeoutException.java
@@ -0,0 +1,9 @@
+package io.netty.channel.jsc;
+public class JSerialCommReadTimeoutException extends RuntimeException{
+    public JSerialCommReadTimeoutException(){
+
+    }
+    public JSerialCommReadTimeoutException(String msg){
+        super(msg);
+    }
+}
